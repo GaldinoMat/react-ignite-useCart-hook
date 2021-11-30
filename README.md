ceeca9838af067a280dded55c1a924610d61e002
Galdino <mateusgaldino1000@hotmail.com>
2021-11-30 10:36:44 -0300

    Finish refactoring code

---
+++ README.md
@@ -0,0 +1,13 @@
+# React Shoppingcart hook
+
+## Objectives
+
+The main objective behind this project was to learn how to use the useContext hook to provide a context to share state and data accross multiple components inside an application.
+
+## Functionalities
+
+Basically the app simulates a sportive shoe store, with a mocked minicart to represent the final checkout. The main challenge of this app was how to properly set the functions inside the shopping cart hook and how to handle the data provided to it. After this the context can be used in alsmot all components, handling the data communication between the shoes' shelf and the checkout.
+
+## What I learned
+
+Besides being considered one of the most basic hooks there is, useContext is quite complicated to grasp at first glance. It differs so much from the usual useState and useEffect hooks, in which can be just declared and used inside their own components, but provide so much versatility when you learn how to properly use it.
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { Product } from '../types';
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,28 +23,82 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart")
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  const cartRef = useRef<Product[]>()
+
+  useEffect(() => {
+    cartRef.current = cart
+  })
+
+  const cartPrevVal = cartRef.current ?? cart
+
+  useEffect(() => {
+    if (cartPrevVal !== cart) {
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(cart));
+    }
+  }, [cart, cartPrevVal])
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart]
+
+      const isAlreadyAdded = updatedCart.find((prod) => (
+        prod.id === productId
+      ));
+
+      const stock = await api.get(`/stock/${productId}`)
+      const stockAmount = stock.data.amount
+
+      const currentAmount = isAlreadyAdded ? isAlreadyAdded.amount : 0
+      const addedAmount = currentAmount + 1
+
+      if (addedAmount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return
+      }
+
+      if (isAlreadyAdded) {
+        isAlreadyAdded.amount = addedAmount
+      } else {
+        const product = await api.get(`/products/${productId}`)
+
+        const newProduct = {
+          ...product.data,
+          amount: 1
+        }
+
+        updatedCart.push(newProduct)
+      }
+      setCart(updatedCart)
     } catch {
-      // TODO
+      toast.error("Erro na adição do produto");
+      return
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart]
+      const removedIndex = updatedCart.findIndex((product) => (productId === product.id)
+      );
+
+      if (removedIndex >= 0) {
+        updatedCart.splice(removedIndex, 1)
+        setCart(updatedCart);
+      } else {
+        throw Error()
+      }
     } catch {
-      // TODO
+      toast.error("Erro na remoção do produto");
+      return
     }
   };
 
@@ -53,9 +107,31 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) return
+
+      const stock = await api.get(`/stock/${productId}`)
+
+      const stockAmount = stock.data.amount;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return
+      }
+
+      const updatedCart = [...cart]
+      const existingProduct = updatedCart.find((prod) => (
+        prod.id === productId
+      ));
+
+      if (existingProduct) {
+        existingProduct.amount = amount
+        setCart(updatedCart)
+      } else {
+        throw Error()
+      }
+
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
